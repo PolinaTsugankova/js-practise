65012b4bdf99d115940d1a1ce180fa260e7d60f5
PolinaTsugankova <pp6056512@gmail.com>
2023-01-28 21:26:11 +0200

    Add some code

---
+++ js/practise.js
@@ -0,0 +1,40 @@
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+// let number = prompt("Write bebra`s name");
+// number = Number(number);
+// console.log(number);
+// console.log(typeof number);
+
+// let height = "25.9px";
+// height = Number.parseFloat(height);
+
+// console.log(height);
+// console.log(typeof height);
+
+// let salary = 10.72382932;
+// salary = Number(salary.toFixed(1));
+
+// console.log(salary);
+// console.log(typeof salary);
+
+// const message = "JavaScript is awesome!";
+// alert(message);
+
+// const base = 300;
+// const quantity = prompt("Введіть суму виручки");
+// const persent = quantity * 0.02;
+// const result = base + persent;
+
+// const message = confirm("result");
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.indexOf("to"));
+// console.log(message.indexOf("hello"));
+
+for (let i = 50; i >= 0; i -= 5) {
+   console.log(i);
+}
